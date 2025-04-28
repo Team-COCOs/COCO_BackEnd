61d725deb25a0c08773691a37ef23e9736d5a3c7
yujeen02 <yujin5479@gmail.com>
2025-04-28 20:12:02 +0900

    feat. nest 기초 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+// typeorm
+import { TypeOrmModule } from "@nestjs/typeorm";
+// config
+import { ConfigModule, ConfigService } from "@nestjs/config";
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "mysql",
+        host: config.get<string>("DB_HOST"),
+        port: parseInt(config.get<string>("DB_PORT")!, 10),
+        username: config.get<string>("DB_USER"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_NAME"),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT!);
 }
 bootstrap();
