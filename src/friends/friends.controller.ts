b2c4cf2f25f2108da058c328471f5d207d37d716
yujeen02 <yujin5479@gmail.com>
2025-05-08 18:49:04 +0900

    fix. 일촌 jwt 수정

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -14,6 +14,7 @@ import { FriendsService } from "./friends.service";
 import { UsersService } from "../users/users.service";
 
 @Controller("friends")
+@UseGuards(AuthGuard("jwt"))
 export class FriendsController {
   constructor(
     private readonly friendsService: FriendsService,
@@ -22,7 +23,6 @@ export class FriendsController {
 
   // 요청 전 이름
   @Get("names/:userId")
-  @UseGuards(AuthGuard("jwt"))
   async getUserNames(@Param("userId") userId: string, @Req() req: any) {
     const requesterId = req.user.id;
     const receiverId = parseInt(userId, 10);
@@ -66,7 +66,6 @@ export class FriendsController {
 
   // 수락
   @Post("accept")
-  @UseGuards(AuthGuard("jwt"))
   async acceptFriend(
     @Body("requesterId") requesterId: number,
     @Req() req: any
@@ -78,7 +77,6 @@ export class FriendsController {
 
   // 거절
   @Post("reject")
-  @UseGuards(AuthGuard("jwt"))
   async rejectFriend(
     @Body("requesterId") requesterId: number,
     @Req() req: any
