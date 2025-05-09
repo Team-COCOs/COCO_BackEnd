ecf05924cf30c2437d46dfaf4b219e6dbf2d27f4
yujeen02 <yujin5479@gmail.com>
2025-05-09 15:35:09 +0900

    feat. 방문자 컨트롤러

--- src/visit/visit.controller.ts
+++ src/visit/visit.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { UsersService } from "src/users/users.service";
+import { VisitService } from "./visit.service";
+import { AuthGuard } from "@nestjs/passport";
 
-@Controller('visit')
-export class VisitController {}
+@Controller("visit")
+export class VisitController {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly visitService: VisitService
+  ) {}
+
+  @Post()
+  @UseGuards(AuthGuard("jwt"))
+  async createVisit(@Body("hostId") hostId: number, @Req() req: any) {
+    const visitorId = req.user.id;
+    return this.visitService.visitOncePerDay(visitorId, hostId);
+  }
+}
