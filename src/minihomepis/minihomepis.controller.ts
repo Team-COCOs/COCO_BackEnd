321ab75bf9e154c737d544ac408dea159859d7a4
yujeen02 <yujin5479@gmail.com>
2025-05-09 18:54:46 +0900

    feat. 방문자 수 카운트

--- src/minihomepis/minihomepis.controller.ts
+++ src/minihomepis/minihomepis.controller.ts
@@ -38,4 +38,6 @@ export class MinihomepisController {
       friends,
     };
   }
+
+  //
 }
--- src/visit/visit.controller.ts
+++ src/visit/visit.controller.ts
@@ -7,9 +7,17 @@ import { AuthGuard } from "@nestjs/passport";
 export class VisitController {
   constructor(private readonly visitService: VisitService) {}
 
-  @Post()
-  async visit(@Body("hostId") hostId: number, @Req() req: any) {
-    const visitorId = req.user?.id ?? null;
+  // 로그인 유저
+  @Post("auth")
+  @UseGuards(AuthGuard("jwt"))
+  async visitAuth(@Body("hostId") hostId: number, @Req() req: any) {
+    const visitorId = req.user.id;
     return this.visitService.visit(hostId, visitorId);
+  }
+
+  // 로그아웃 유저
+  @Post("guest")
+  async visitGuest(@Body("hostId") hostId: number) {
+    return this.visitService.visit(hostId, null);
   }
 }
