07959f69f2b961c760e3c214d9dc1bfa8790de3a
yujeen02 <yujin5479@gmail.com>
2025-05-12 16:55:23 +0900

    fix. 미니홈피 정보 조회

--- src/minihomepis/minihomepis.service.ts
+++ src/minihomepis/minihomepis.service.ts
@@ -63,7 +63,14 @@ export class MinihomepisService {
       where: { user: { id: userId } },
     });
 
-    if (!mini) throw new Error("미니홈피가 존재하지 않습니다.");
+    if (!mini) {
+      return {
+        title: null,
+        mood: null,
+        introduction: null,
+        minihompi_image: null,
+      };
+    }
 
     return {
       title: mini.title,
