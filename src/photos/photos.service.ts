bcead4144b23937474bf9a8d8e3f409e750816cb
yujeen02 <yujin5479@gmail.com>
2025-05-16 18:34:23 +0900

    fix. 스크랩 원본자 이름

--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -266,7 +266,8 @@ export class PhotosService {
     copiedPhoto.folder = scrapFolder;
     copiedPhoto.isScripted = true;
     copiedPhoto.user = user;
-    copiedPhoto.origin_author = originalPhoto.user.name;
+    copiedPhoto.origin_author =
+      originalPhoto.origin_author || originalPhoto.user.name;
 
     originalPhoto.use_count = (originalPhoto.use_count || 0) + 1;
     await this.photoRepository.save(originalPhoto);
