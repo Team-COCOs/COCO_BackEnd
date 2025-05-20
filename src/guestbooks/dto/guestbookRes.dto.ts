ff138409b63439a5b8346c6fa71365d8518bde55
yujeen02 <yujin5479@gmail.com>
2025-05-20 16:56:43 +0900

    fix. 방명록 dto 수정

--- src/guestbooks/dto/guestbookRes.dto.ts
+++ src/guestbooks/dto/guestbookRes.dto.ts
@@ -5,8 +5,18 @@ export interface GuestbookResponseDto {
   authorId: number;
   hostId: number;
   authorRealName: string;
+  authorProfile: string;
+  authorGender: string;
   hostRealName: string;
   content: string;
   status: VisibilityStatus;
   created_at: string;
+  isMine: boolean;
+  comments: {
+    id: number;
+    content: string;
+    authorId: number;
+    authorName: string;
+    created_at: string;
+  }[];
 }
