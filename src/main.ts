bc59c2138588f16eb579a7edc06f9dd26c452f53
yujeen02 <yujin5479@gmail.com>
2025-05-21 15:51:52 +0900

    fix. 프론트 코드

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://34.236.72.45"],
     credentials: true,
   });
 
